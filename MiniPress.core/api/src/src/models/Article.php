d1dab526e522c016024f1f33d299a443ac44e792
Stanislas JEANGEY <jeangey1u@etu.univ-lorraine.fr>
2023-06-14 16:07:53 +0200

    squelette ajout

---
+++ MiniPress.core/api/src/src/models/Article.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+class Article extends Model
+{
+    protected $table = 'articles';
+    protected $fillable = ['titre', 'resume', 'contenu', 'date_creation', 'image_url', 'categorie_id', 'auteur_id'];
+
+    public function categorie()
+    {
+        return $this->belongsTo(Categorie::class, 'categorie_id');
+    }
+
+    public function auteur()
+    {
+        return $this->belongsTo(Auteur::class, 'auteur_id');
+    }
+
+}
