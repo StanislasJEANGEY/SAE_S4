dc247b9d21e17d7d129bf20d1bd3f5de42572a65
Antonin <anto10800@gmail.com>
2023-06-15 16:48:57 +0200

    update routes.php

--- MiniPress.core/appli/src/conf/routes.php
+++ MiniPress.core/appli/src/conf/routes.php
@@ -2,6 +2,10 @@
 
 
 use minipress\appli\actions\InscriptionAction;
+use minipress\api\actions\getArticleByIdByApi;
+use minipress\api\actions\getArticlesByCategorie;
+use minipress\api\actions\getAuteurById;
+use minipress\api\actions\getCategoriesByApi;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -11,8 +15,9 @@ return function (\Slim\App $app): void {
 <a href="/inscription">Inscription</a>');
         return $response;
     })->setName('home');
-    $app->get('/inscription[/]', InscriptionAction::class)->setName("inscription_get");
-    $app->post('/inscription[/]', InscriptionAction::class)->setName("inscription_post");
+    $app->get('/appli/setArticleByCategorie',  minipress\appli\actions\set\setArticleByCategorie::class)->setName('setArticleByCategorie');
+    $app->get('/inscription[/]', \App\Actions\InscriptionAction::class)->setName("inscription_get");
+    $app->post('/inscription[/]', \App\Actions\InscriptionAction::class)->setName("inscription_post");
     $app->get('/connexion[/]', \minipress\appli\actions\ConnexionAction::class)->setName("connexion_get");
     $app->post('/connexion[/]', \minipress\appli\actions\ConnexionAction::class)->setName("connexion_post");
 };
