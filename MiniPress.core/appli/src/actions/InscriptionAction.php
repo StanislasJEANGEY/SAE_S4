7e8053a356a303015389b8bfe79f4f4edcca0eda
Dami1 <damien.melchior3@etu.univ-lorraine.fr>
2023-06-15 16:27:54 +0200

    fix routes

--- MiniPress.core/appli/src/actions/InscriptionAction.php
+++ MiniPress.core/appli/src/actions/InscriptionAction.php
@@ -2,8 +2,6 @@
 
 namespace minipress\appli\actions;
 
-namespace App\Actions;
-
 use minipress\appli\actions\AbstractAction;
 use minipress\appli\models\User;
 use minipress\appli\services\auth\AuthentificationService;
--- MiniPress.core/appli/src/conf/routes.php
+++ MiniPress.core/appli/src/conf/routes.php
@@ -1,16 +1,18 @@
 <?php
 
 
+use minipress\appli\actions\InscriptionAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (\Slim\App $app): void {
     $app->get('/', function (Request $request, Response $response, array $args) {
-        $response->getBody()->write('<center><h1>Minipress</h1></center>');
+        $response->getBody()->write('<center><h1>Minipress</h1></center>
+<a href="/inscription">Inscription</a>');
         return $response;
     })->setName('home');
-    $app->get('/inscription[/]', \App\Actions\InscriptionAction::class)->setName("inscription_get");
-    $app->post('/inscription[/]', \App\Actions\InscriptionAction::class)->setName("inscription_post");
+    $app->get('/inscription[/]', InscriptionAction::class)->setName("inscription_get");
+    $app->post('/inscription[/]', InscriptionAction::class)->setName("inscription_post");
     $app->get('/connexion[/]', \minipress\appli\actions\ConnexionAction::class)->setName("connexion_get");
     $app->post('/connexion[/]', \minipress\appli\actions\ConnexionAction::class)->setName("connexion_post");
 };
