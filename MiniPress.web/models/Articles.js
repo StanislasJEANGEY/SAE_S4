b81dfa624666e8d96154d74d4a989d2b28830ec8
Antonin <anto10800@gmail.com>
2023-06-20 14:47:24 +0200

    modif Articles.js

--- MiniPress.web/models/Articles.js
+++ MiniPress.web/models/Articles.js
@@ -1,15 +1,31 @@
+import * as loader from './../lib/loader.js';
 
 export default class Articles {
-     tabArticles = [];
+    _tabArticles = [];
+    _count = 0;
 
 
     constructor(data) {
-        this.tabArticles = data;
+        this._tabArticles = data['article'];
+        this._count = data['count'];
     }
 
-
-    toString() {
-        console.log(this.tabArticles);
+    filtreArticlesByMotCle(motCle) {
+        let tab = [];
+        this._tabArticles.forEach((article) => {
+            if (article['titre'].includes(motCle)) {
+                tab.push(article);
+            }
+        });
+        console.log(tab);
+        return tab;
     }
 
+    get tabArticles() {
+        return this._tabArticles;
+    }
+
+    get count() {
+        return this._count;
+    }
 }
