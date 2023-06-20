9a03fc9a774c8fa92f445b450b58610bb2afd199
Antonin <anto10800@gmail.com>
2023-06-20 17:16:12 +0200

    ajout du markdown dans le scriptBase.sql

--- sql/scriptBase.sql
+++ sql/scriptBase.sql
@@ -70,19 +70,137 @@ VALUES ('admin', 1);
 
 -- Insertion d'articles
 INSERT INTO articles (titre, resume, contenu, date_creation, image_url, categorie_id, auteur_id, publie)
-VALUES ('Article 1', 'Résumé de l\'article 1', 'Contenu de l\'article 1...', '2023-06-01 10:00:00',
+VALUES (' Article 1', '## Résumé
+Dans cet article, nous explorons les fonctionnalités de Markdown et comment les utiliser pour formater du contenu. Découvrez comment ajouter des titres, créer des listes, insérer des liens et des images, et bien plus encore !
+', '
+### Paragraphe 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat massa eu ex aliquet, eget consequat nunc varius. Sed ac malesuada turpis. *Italique* et **gras** peuvent être utilisés pour mettre en valeur certaines parties du texte. Par exemple, vous pouvez créer une liste non ordonnée pour résumer vos points principaux :
+
+- Fonctionnalité 1
+- Fonctionnalité 2
+- Fonctionnalité 3
+
+### Paragraphe 2
+Maecenas ac est malesuada, rutrum quam in, eleifend eros. Mauris sit amet enim eu lacus cursus efficitur. Vous pouvez également créer des liens vers des ressources externes. Par exemple, voici un [lien vers notre site Web](https://www.example.com) où vous pouvez en apprendre davantage sur ces fonctionnalités.
+
+### Paragraphe 3
+Vestibulum et aliquet massa, quis commodo nisl. Fusce tincidunt, risus in dignissim semper, leo eros accumsan nulla, at viverra tellus tellus sed odio. Vous pouvez insérer des images dans votre contenu en utilisant la syntaxe suivante :
+
+', '2023-06-01 10:00:00',
         'https://exemple.com/image1.jpg', 1, 1, 1),
-       ('Article 2', 'Résumé de l\'article 2', 'Contenu de l\'article 2...', '2023-06-02 14:30:00',
-        'https://exemple.com/image2.jpg', 2, 1, 1),
-       ('Article 3', 'Résumé de l\'article 3', 'Contenu de l\'article 3...', '2023-06-03 16:45:00',
-        'https://exemple.com/image3.jpg', 3, 1, 1),
-       ('Article 4', 'Résumé de l\'article 4', 'Contenu de l\'article 4...', '2023-06-04 09:15:00',
-        'https://exemple.com/image4.jpg', 1, 1, 0),
-       ('Article 5', 'Résumé de l\'article 5', 'Contenu de l\'article 5...', '2023-06-05 11:30:00',
-        'https://exemple.com/image5.jpg', 2, 1, 0),
-       ('Article 6', 'Résumé de l\'article 6', 'Contenu de l\'article 6...', '2023-06-06 13:45:00',
-        'https://exemple.com/image6.jpg', 3, 1, 0),
-       ('Article 7', 'Résumé de l\'article 7', 'Contenu de l\'article 7...', '2023-06-07 15:00:00',
-        'https://exemple.com/image7.jpg', 1, 1, 0),
-       ('Article 8', 'Résumé de l\'article 8', 'Contenu de l\'article 8...', '2023-06-08 17:15:00',
-        'https://exemple.com/image8.jpg', 2, 1, 0);
+
+       (' Article 98', '## Résumé
+Dans cet article, nous explorons les fonctionnalités de Markdown et comment les utiliser pour formater du contenu. Découvrez comment ajouter des titres, créer des listes, insérer des liens et des images, et bien plus encore !
+', '
+### Paragraphe 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat massa eu ex aliquet, eget consequat nunc varius. Sed ac malesuada turpis. *Italique* et **gras** peuvent être utilisés pour mettre en valeur certaines parties du texte. Par exemple, vous pouvez créer une liste non ordonnée pour résumer vos points principaux :
+
+- Fonctionnalité 1
+- Fonctionnalité 2
+- Fonctionnalité 3
+
+### Paragraphe 2
+Maecenas ac est malesuada, rutrum quam in, eleifend eros. Mauris sit amet enim eu lacus cursus efficitur. Vous pouvez également créer des liens vers des ressources externes. Par exemple, voici un [lien vers notre site Web](https://www.example.com) où vous pouvez en apprendre davantage sur ces fonctionnalités.
+
+### Paragraphe 3
+Vestibulum et aliquet massa, quis commodo nisl. Fusce tincidunt, risus in dignissim semper, leo eros accumsan nulla, at viverra tellus tellus sed odio. Vous pouvez insérer des images dans votre contenu en utilisant la syntaxe suivante :
+
+', '2023-06-01 11:00:00',
+        'https://exemple.com/image2.jpg', 1, 1, 1),
+
+       (' Article 2', '## Résumé
+Dans cet article, nous explorons les fonctionnalités de Markdown et comment les utiliser pour formater du contenu. Découvrez comment ajouter des titres, créer des listes, insérer des liens et des images, et bien plus encore !
+', '
+### Paragraphe 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat massa eu ex aliquet, eget consequat nunc varius. Sed ac malesuada turpis. *Italique* et **gras** peuvent être utilisés pour mettre en valeur certaines parties du texte. Par exemple, vous pouvez créer une liste non ordonnée pour résumer vos points principaux :
+
+- Fonctionnalité 1
+- Fonctionnalité 2
+- Fonctionnalité 3
+
+### Paragraphe 2
+Maecenas ac est malesuada, rutrum quam in, eleifend eros. Mauris sit amet enim eu lacus cursus efficitur. Vous pouvez également créer des liens vers des ressources externes. Par exemple, voici un [lien vers notre site Web](https://www.example.com) où vous pouvez en apprendre davantage sur ces fonctionnalités.
+
+### Paragraphe 3
+Vestibulum et aliquet massa, quis commodo nisl. Fusce tincidunt, risus in dignissim semper, leo eros accumsan nulla, at viverra tellus tellus sed odio. Vous pouvez insérer des images dans votre contenu en utilisant la syntaxe suivante :
+
+', '2023-06-01 12:00:00',
+        'https://exemple.com/image3.jpg', 1, 1, 1),
+
+       (' Article 4', '## Résumé
+Dans cet article, nous explorons les fonctionnalités de Markdown et comment les utiliser pour formater du contenu. Découvrez comment ajouter des titres, créer des listes, insérer des liens et des images, et bien plus encore !
+', '
+### Paragraphe 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat massa eu ex aliquet, eget consequat nunc varius. Sed ac malesuada turpis. *Italique* et **gras** peuvent être utilisés pour mettre en valeur certaines parties du texte. Par exemple, vous pouvez créer une liste non ordonnée pour résumer vos points principaux :
+
+- Fonctionnalité 1
+- Fonctionnalité 2
+- Fonctionnalité 3
+
+### Paragraphe 2
+Maecenas ac est malesuada, rutrum quam in, eleifend eros. Mauris sit amet enim eu lacus cursus efficitur. Vous pouvez également créer des liens vers des ressources externes. Par exemple, voici un [lien vers notre site Web](https://www.example.com) où vous pouvez en apprendre davantage sur ces fonctionnalités.
+
+### Paragraphe 3
+Vestibulum et aliquet massa, quis commodo nisl. Fusce tincidunt, risus in dignissim semper, leo eros accumsan nulla, at viverra tellus tellus sed odio. Vous pouvez insérer des images dans votre contenu en utilisant la syntaxe suivante :
+
+', '2023-06-01 19:45:00',
+        'https://exemple.com/image98.jpg', 1, 1, 1),
+
+       (' Article 17', '## Résumé
+Dans cet article, nous explorons les fonctionnalités de Markdown et comment les utiliser pour formater du contenu. Découvrez comment ajouter des titres, créer des listes, insérer des liens et des images, et bien plus encore !
+', '
+### Paragraphe 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat massa eu ex aliquet, eget consequat nunc varius. Sed ac malesuada turpis. *Italique* et **gras** peuvent être utilisés pour mettre en valeur certaines parties du texte. Par exemple, vous pouvez créer une liste non ordonnée pour résumer vos points principaux :
+
+- Fonctionnalité 1
+- Fonctionnalité 2
+- Fonctionnalité 3
+
+### Paragraphe 2
+Maecenas ac est malesuada, rutrum quam in, eleifend eros. Mauris sit amet enim eu lacus cursus efficitur. Vous pouvez également créer des liens vers des ressources externes. Par exemple, voici un [lien vers notre site Web](https://www.example.com) où vous pouvez en apprendre davantage sur ces fonctionnalités.
+
+### Paragraphe 3
+Vestibulum et aliquet massa, quis commodo nisl. Fusce tincidunt, risus in dignissim semper, leo eros accumsan nulla, at viverra tellus tellus sed odio. Vous pouvez insérer des images dans votre contenu en utilisant la syntaxe suivante :
+
+', '2023-06-01 10:56:00',
+        'https://exemple.com/image1.jpg', 1, 1, 1),
+
+       ('Article 78', '## Résumé
+Dans cet article, nous explorons les fonctionnalités de Markdown et comment les utiliser pour formater du contenu. Découvrez comment ajouter des titres, créer des listes, insérer des liens et des images, et bien plus encore !
+', '
+### Paragraphe 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat massa eu ex aliquet, eget consequat nunc varius. Sed ac malesuada turpis. *Italique* et **gras** peuvent être utilisés pour mettre en valeur certaines parties du texte. Par exemple, vous pouvez créer une liste non ordonnée pour résumer vos points principaux :
+
+- Fonctionnalité 1
+- Fonctionnalité 2
+- Fonctionnalité 3
+
+### Paragraphe 2
+Maecenas ac est malesuada, rutrum quam in, eleifend eros. Mauris sit amet enim eu lacus cursus efficitur. Vous pouvez également créer des liens vers des ressources externes. Par exemple, voici un [lien vers notre site Web](https://www.example.com) où vous pouvez en apprendre davantage sur ces fonctionnalités.
+
+### Paragraphe 3
+Vestibulum et aliquet massa, quis commodo nisl. Fusce tincidunt, risus in dignissim semper, leo eros accumsan nulla, at viverra tellus tellus sed odio. Vous pouvez insérer des images dans votre contenu en utilisant la syntaxe suivante :
+
+', '2023-06-15 13:00:00',
+        'https://exemple.com/image1.jpg', 1, 1, 1),
+
+       (' Article 6', '## Résumé
+Dans cet article, nous explorons les fonctionnalités de Markdown et comment les utiliser pour formater du contenu. Découvrez comment ajouter des titres, créer des listes, insérer des liens et des images, et bien plus encore !
+', '
+### Paragraphe 1
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat massa eu ex aliquet, eget consequat nunc varius. Sed ac malesuada turpis. *Italique* et **gras** peuvent être utilisés pour mettre en valeur certaines parties du texte. Par exemple, vous pouvez créer une liste non ordonnée pour résumer vos points principaux :
+
+- Fonctionnalité 1
+- Fonctionnalité 2
+- Fonctionnalité 3
+
+### Paragraphe 2
+Maecenas ac est malesuada, rutrum quam in, eleifend eros. Mauris sit amet enim eu lacus cursus efficitur. Vous pouvez également créer des liens vers des ressources externes. Par exemple, voici un [lien vers notre site Web](https://www.example.com) où vous pouvez en apprendre davantage sur ces fonctionnalités.
+
+### Paragraphe 3
+Vestibulum et aliquet massa, quis commodo nisl. Fusce tincidunt, risus in dignissim semper, leo eros accumsan nulla, at viverra tellus tellus sed odio. Vous pouvez insérer des images dans votre contenu en utilisant la syntaxe suivante :
+
+', '2023-07-01 10:00:00',
+        'https://exemple.com/image1.jpg', 1, 1, 0)
+
+
